e73f720b8f9a058bb5835b243989b6a626f5c98d
Захар Домолего <DomolegoZahar@gmail.com>
2021-09-25 15:09:43 +0800

    Добавил руководство пользователя в раздел О программе

--- LAB_2/Form2.Designer.cs
+++ LAB_2/Form2.Designer.cs
@@ -29,14 +29,26 @@ namespace LAB_2
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(programmeInfo));
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(130, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 360);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // programmeInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "programmeInfo";
@@ -46,5 +58,7 @@ namespace LAB_2
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
