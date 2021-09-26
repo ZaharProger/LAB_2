21680530f8c45882d8b72977b87c29682acac1f7
Захар Домолего <DomolegoZahar@gmail.com>
2021-09-26 11:38:29 +0800

    Добавил классы времени и результата программы

---
+++ LAB_2/ProgrammeResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB_2
+{
+    //Результат программы
+    public class ProgrammeResult
+    {
+        //Сообщение (для пользователя)
+        private string message;
+        //Булевый результат (для программной логики)
+        private bool isSuccessfully;
+
+        public ProgrammeResult()
+        {
+            message = "Не были получены входные данные для расчёта";
+            isSuccessfully = false;
+        }
+
+        public ProgrammeResult(string message, bool isSuccessfully)
+        {
+            this.message = message;
+            this.isSuccessfully = isSuccessfully;
+        }
+
+        public void SetMessage(string message)
+        {
+            this.message = message;
+        }
+
+        public void SetSuccessStatus(bool status)
+        {
+            isSuccessfully = status;
+        }
+
+        public string GetMessage()
+        {
+            return message;
+        }
+
+        public bool GetSuccessStatus()
+        {
+            return isSuccessfully;
+        }
+    }
+}
---
+++ LAB_2/Time.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB_2
+{
+    //Время
+    class Time
+    {
+        //часы
+        private short hours;
+        //минуты
+        private short minutes;
+
+        public Time(short hours, short minutes)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+
+        public void SetHours(short hours)
+        {
+            this.hours = hours;
+        }
+
+        public void setMinutes(short minutes)
+        {
+            this.minutes = minutes;
+        }
+
+        public short GetHours()
+        {
+            return hours;
+        }
+
+        public short GetMinutes()
+        {
+            return minutes;
+        }
+    }
+}
